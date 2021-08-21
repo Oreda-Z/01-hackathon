3f78293ce6e2266c93cbf62760692f7e6db858c4
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-08-21 04:53:49 +0200

    Implemented timer module

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,13 @@
-export function random(min, max) {
-	return Math.round(min - 0.5 + Math.random() * (max - min + 1))
-}
+export const random = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1))
 
-export function randomColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+export const randomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
+
+export const addZero = (number) => (number < 10) ? `0${number}` : number
+
+export const formatSeconds = sec => {
+	const hours = addZero(Math.floor(sec / (60 * 60)))
+	const minutes = addZero(Math.floor((sec % (60 * 60)) / 60))
+	const seconds = addZero(Math.ceil((sec % (60 * 60)) % 60))
+
+	return `${hours}:${minutes}:${seconds}`
 }
