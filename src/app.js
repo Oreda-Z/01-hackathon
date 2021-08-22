b3d12e372867c0bf98758821455db41fc165fbeb
Oreda-Z <shtucer31@gmail.com>
2021-08-22 15:03:28 +0300

    random sound

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import {StopWatchModule} from "./modules/stopwatch.module";
 import './styles.css'
 import {SuperCounter} from "./modules/superCounter";
 import {RandomMessage} from "./modules/randomMessage";
+import {RandomSound} from "./modules/randomSound";
 
 const contextMenu = new ContextMenu('.menu')
 
@@ -17,13 +18,14 @@ document.addEventListener('click', event => {
 	contextMenu.close()
 })
 
-const shapeModule = new ShapeModule('randomFigure', 'Случайная фигура')
-const timerModule = new TimerModule('timer', 'Таймер')
-const backgroundModule = new BackgroundModule('randomBackground', 'Сменить фон')
-const stopwatchModule = new StopWatchModule('stopWatch', 'Секундомер')
-const superCounter = new SuperCounter('superCounter', 'Супер счетчик')
+const shapeModule = new ShapeModule('randomFigure', 'Случайная фигура');
+const timerModule = new TimerModule('timer', 'Таймер');
+const backgroundModule = new BackgroundModule('randomBackground', 'Сменить фон');
+const stopwatchModule = new StopWatchModule('stopWatch', 'Секундомер');
+const superCounter = new SuperCounter('superCounter', 'Супер счетчик');
 const randomMessage = new RandomMessage('randomMessage', 'Случайное сообщение');
-const arrWithModules = [shapeModule, timerModule, backgroundModule, stopwatchModule, superCounter, randomMessage]
-contextMenu.add(arrWithModules)
+const randomSound = new RandomSound('randomSound', 'случайный звук');
+const arrWithModules = [shapeModule, timerModule, backgroundModule, stopwatchModule, superCounter, randomMessage, randomSound];
+contextMenu.add(arrWithModules);
 
 
