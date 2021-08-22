3ff71b9a1cd187ee542e3ed84933c49f2f054132
Oreda-Z <shtucer31@gmail.com>
2021-08-22 13:34:13 +0300

    hackathon v1.1

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import {ShapeModule} from './modules/shape.module'
 import {StopWatchModule} from "./modules/stopwatch.module";
 
 import './styles.css'
+import {SuperCounter} from "./modules/superCounter";
 
 const contextMenu = new ContextMenu('.menu')
 
@@ -19,7 +20,8 @@ const shapeModule = new ShapeModule('randomFigure', 'Случайная фигу
 const timerModule = new TimerModule('timer', 'Таймер')
 const backgroundModule = new BackgroundModule('randomBackground', 'Сменить фон')
 const stopwatchModule = new StopWatchModule('stopWatch', 'Секундомер')
-const arrWithModules = [shapeModule, timerModule, backgroundModule, stopwatchModule]
+const superCounter = new SuperCounter('superCounter', 'Супер счетчик')
+const arrWithModules = [shapeModule, timerModule, backgroundModule, stopwatchModule, superCounter]
 contextMenu.add(arrWithModules)
 
 
